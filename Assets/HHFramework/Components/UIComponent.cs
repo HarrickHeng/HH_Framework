beaef3c57be1e5a0975112bfc511aa2243b94ca4
HarrickHeng <chenshengheng@hotmail.com>
2022-11-25 20:32:34 +0800

    test gitea
    test gitea

--- Assets/HHFramework/Components/UIComponent.cs
+++ Assets/HHFramework/Components/UIComponent.cs
@@ -1,3 +1,7 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace HHFramework
 {
     /// <summary>
@@ -5,11 +9,49 @@ namespace HHFramework
     /// </summary>
     public class UIComponent : HHBaseComponent, IUpdateComponent
     {
+        [Header("标准分辨率宽度")] [SerializeField] private int mStandardWidth = 1280;
+
+        [Header("标准分辨率高度")] [SerializeField] private int mStandardHeight = 720;
+
+        [Header("UI摄像机")] public Camera UICamera;
+
+        [Header("跟画布的缩放")] [SerializeField] private CanvasScaler mUIRootCanvasScaler;
+
         protected override void OnAwake()
         {
             base.OnAwake();
             GameEntry.RegisterUpdateComponent(this);
         }
+
+        #region UI适配
+
+        /// <summary>
+        /// 自动适配缩放
+        /// </summary>
+        public void AutoCanvasScaler()
+        {
+            var standardScreen = mStandardWidth / (float)mStandardHeight;
+            var currScreen = Screen.width / (float)Screen.height;
+
+            if (currScreen > standardScreen)
+            {
+                mUIRootCanvasScaler.matchWidthOrHeight = 0;
+            }
+            else
+            {
+                mUIRootCanvasScaler.matchWidthOrHeight = standardScreen - currScreen;
+            }
+        }
+
+        /// <summary>
+        /// 设置适配缩放为1
+        /// </summary>
+        public void SetCanvasScaler()
+        {
+            mUIRootCanvasScaler.matchWidthOrHeight = 1;
+        }
+
+        #endregion
 
         public override void ShutDown()
         {
